d652d4172dc428ae873064a51e70dcc678744422
yj2213 <isabel031113@gmail.com>
2025-02-08 20:39:46 -0500

    YJ commit 1

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 const GameScreen = () => {
+  const location = useLocation();
+  // Do not render the top UI of GameScreen when the current path is /game/play
+  const isGamePlay = location.pathname === '/game/play';
+
   return (
     <div style={{ textAlign: 'center', marginTop: '3rem' }}>
-      <h2>Game</h2>
-      <p>This is the game section. Choose an option from above.</p>
+      {!isGamePlay && (
+        <>
+          <h2>Game</h2>
+          <p>This is the game section. Choose an option from above.</p>
+        </>
+      )}
       <Outlet />
     </div>
   );
